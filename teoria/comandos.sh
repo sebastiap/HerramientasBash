d42fd8d64267a372eb9ea5a3463b8cea5cfdf37a
Sebastian Nicolas Picardi <36540321+sebastiap@users.noreply.github.com>
2022-12-05 17:00:31 -0300

    Create comandos.sh

---
+++ teoria/comandos.sh
@@ -0,0 +1,15 @@
+# Comandos
+
+## Comandos de la linea de comandos
+* head - Muestra las primeras 10 lineas de un archivo
+* tail - Muestra las primeras 10 lineas de un archivo
+* wc - cuanta la cantidad de lineas , palabras y caracteres 
+-l lineas
+-w palabras 
+-m caracteres
+
+sort - ordena alfabeticamente
+uniq - no muestra lineas repetidas
+ip addr show - Muestra nuestro ip
+
+## comando grep
